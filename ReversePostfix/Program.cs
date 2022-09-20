48e1d479661b4aa3957635dcb7c37f4d802e05a7
Enrico Rossini <enrico.rossini.uk@live.com>
2022-09-20 10:01:34 +0100

    First upload

---
+++ ReversePostfix/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReversePostfix
+{
+	public class Test
+	{
+		public static void Main()
+		{
+			RunTestCase(10, new[] { "5", "2", "*" }); // 5 * 2 = 10
+			RunTestCase(13, new[] { "10", "2", "*", "6", "+", "2", "/" }); // ((10 * 2) + 6) / 2 = 13
+			RunTestCase(95, new[] { "100", "10", "-", "30", "6", "/", "+" }); // (100 - 10) + (30 / 6) = 95
+			RunTestCase(6, new[] { "10", "7", "-", "!" }); // (10 - 7)! = 6 
+		}
+
+		public static double Calculate(IEnumerable<string> input)
+		{
+			Stack<double> stack = new Stack<double>();
+
+			foreach (string element in input)
+			{
+				if (!"+-*/!".Contains(element))
+				{
+					stack.Push(Convert.ToDouble(element));
+					continue;
+				}
+
+				double second = stack.Pop();
+				double first = stack.Count() > 0 ? stack.Pop() : 0;
+				double ans = 0;
+				switch (element)
+				{
+					case "+":
+						ans = first + second;
+						break;
+					case "-":
+						ans = first - second;
+						break;
+					case "/":
+						ans = first / second;
+						break;
+					case "*":
+						ans = first * second;
+						break;
+					case "!":
+						ans = second * 2;
+						break;
+
+				}
+				stack.Push(ans);
+			}
+			return stack.Pop();
+		}
+
+		// ---------------------------------------------------------------------------
+		public static double CalculateFactorial(double input)
+		{
+			if (input > 100)
+			{
+				throw new InvalidOperationException("This will take too long for the interview.");
+			}
+
+			var num = input;
+			for (var i = 1; i < input; ++i)
+			{
+				num *= input - i;
+			}
+
+			return num;
+		}
+
+		public static void RunTestCase(double expectedResult, IEnumerable<string> input)
+		{
+			var result = Calculate(input);
+			if (Math.Abs(result - expectedResult) < 0.001)
+			{
+				Console.WriteLine("Pass");
+			}
+			else
+			{
+				Console.WriteLine("Fail: Expected = {0}, Actual = {1}. Input = '{2}'", expectedResult, result, string.Join(",", input));
+			}
+		}
+	}
+}
